a16965ed3d2c0020c9695e2f849dbff291e01b5d
Brianleach11 <81440466+Brianleach11@users.noreply.github.com>
2022-07-26 17:06:42 -0400

    Update API Endpoint

--- Client/Static/APIEndpoint.cs
+++ Client/Static/APIEndpoint.cs
@@ -5,7 +5,7 @@ internal static class APIEndpoint
     #if DEBUG
         internal const string ServerBaseUrl = "https://localhost:5003";
     #else
-        internal const string ServerBaseUrl = "https://appname.azurewebsite.net";
+        internal const string ServerBaseUrl = "http://briansdevportfolio.azurewebsites.net";
     #endif
 
     internal readonly static string s_categories = $"{ServerBaseUrl}/api/categories";
